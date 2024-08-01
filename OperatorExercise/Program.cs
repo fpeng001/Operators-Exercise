b267ac73ff60cc2676f1ada103dffc75040a3dc2
Fu-Lin Peng <fulinpeng53@gmail.com>
2024-08-01 18:07:00 -0400

    initial commit

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 17;
+            int b = 4;
+            var sum = a + b;
+            var diff = a - b;
+            var product = a * b;
+            var quotient = a / b;
+            var remainder = a % b;
+
+            Console.WriteLine($"{a} + {b} is {sum}. \n " +
+                $"{a} - {b} is {diff}. \n" +
+                $"{a} * {b} is {product}. \n" +
+                $"{a} / {b} is {quotient} remainder {remainder}.");
         }
     }
 }
