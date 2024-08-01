6d8aeae8be88c7352ea8c0cd250667c609754c9b
Fu-Lin Peng <fulinpeng53@gmail.com>
2024-08-01 18:25:31 -0400

    Exercise 2 added

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -2,8 +2,23 @@
 {
     public class Program
     {
+        public static double AreaOfCircle(double radius)
+        {
+            var area = Math.PI * radius * radius;
+            return area;
+        }
         static void Main(string[] args)
         {
+
+            Console.WriteLine("What is the radius of your circle?");
+            var userInput = Console.ReadLine();
+            var radius = double.Parse(userInput);
+
+            AreaOfCircle(radius);
+
+            Console.WriteLine(AreaOfCircle(radius));
+
+
             int a = 17;
             int b = 4;
             var sum = a + b;
